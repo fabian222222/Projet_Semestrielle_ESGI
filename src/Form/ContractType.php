f9d478ab55d837e171b64131bb0c29ac051c4aad
Sébastien <sebramirez93100@gmail.com>
2023-12-11 23:40:53 +0100

    [AE-23] : Création du CRUD pour les contrats

---
+++ src/Form/ContractType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Contract;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContractType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('price')
+            ->add('description')
+            ->add('validityDate')
+            ->add('client')
+            ->add('drivingSchool')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Contract::class,
+        ]);
+    }
+}
