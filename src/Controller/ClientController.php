d2fb4d9b8f0f9f11bf86a203384c4785bb5af9f9
Sébastien <sebramirez93100@gmail.com>
2023-12-16 21:54:57 +0100

    [AE-55] : - Ajoute le CRUD de client en fonction de la drivingSchool

---
+++ src/Controller/ClientController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Client;
+use App\Entity\DrivingSchool;
+use App\Form\ClientType;
+use App\Repository\ClientRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('driving/school/{idS}/client')]
+class ClientController extends AbstractController
+{
+    #[Route('/', name: 'app_client_index', methods: ['GET'])]
+    public function index(ClientRepository $clientRepository, DrivingSchool $idS): Response
+    {
+        return $this->render('client/index.html.twig', [
+            'clients' => $clientRepository->findAll(),
+            'drivingSchool' => $idS->getId(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_client_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager, DrivingSchool $idS): Response
+    {
+        $client = new Client();
+        $form = $this->createForm(ClientType::class, $client);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $client->setDrivingSchool($idS);
+            $entityManager->persist($client);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_client_index', ["idS" => $idS->getId()], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('client/new.html.twig', [
+            'drivingSchool' => $idS,
+            'client' => $client,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_client_show', methods: ['GET'])]
+    public function show(Client $client, DrivingSchool $idS): Response
+    {
+        return $this->render('client/show.html.twig', [
+            'client' => $client,
+            'drivingSchool' => $idS->getId(),
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_client_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Client $client, EntityManagerInterface $entityManager, DrivingSchool $idS): Response
+    {
+        $form = $this->createForm(ClientType::class, $client);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_client_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('client/edit.html.twig', [
+            'client' => $client,
+            'form' => $form,
+            'drivingSchool' => $idS->getId(),
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_client_delete', methods: ['POST'])]
+    public function delete(Request $request, Client $client, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$client->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($client);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_client_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
