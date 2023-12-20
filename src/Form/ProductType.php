908f9399224e0eaad8ccff95ebb7b2d1fdfbe6f3
fabian222222 <fabian.zuo@supinternet.fr>
2023-12-20 11:31:41 +0100

    fix : adapt product form after changes

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -9,6 +9,8 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\TimeType;
 
 class ProductType extends AbstractType
 {
