f4fd84fc33dce958ab7959d25548dde71cdc7b31
Sébastien <sebramirez93100@gmail.com>
2024-02-16 00:50:34 +0100

    AE-87 : Ajoute champs auto école

--- migrations/Version20240215233041.php
+++ migrations/Version20240215233041.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240127132417 extends AbstractMigration
+final class Version20240215233041 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,11 +20,17 @@ final class Version20240127132417 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE driving_school ALTER number SET NOT NULL');
+        $this->addSql('ALTER TABLE driving_school ALTER city SET NOT NULL');
+        $this->addSql('ALTER TABLE driving_school ALTER zip_code SET NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE driving_school ALTER number DROP NOT NULL');
+        $this->addSql('ALTER TABLE driving_school ALTER city DROP NOT NULL');
+        $this->addSql('ALTER TABLE driving_school ALTER zip_code DROP NOT NULL');
     }
 }
