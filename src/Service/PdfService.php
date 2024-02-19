dec55d4f5150faddb87099f5a0ed760398eb911a
Sébastien <sebramirez93100@gmail.com>
2024-02-19 20:24:52 +0100

    AE-77 : Ajoute pdf pour les contrats

--- src/Service/PdfService.php
+++ src/Service/PdfService.php
@@ -5,16 +5,19 @@ namespace App\Service;
 require '../vendor/autoload.php';
 
 use Dompdf\Dompdf;
+use Dompdf\Options;
 
 class PdfService
 {
     public function __construct()
     {
+//        $pdfOptions = new Options();
+//        $pdfOptions->setChroot("/public/images");
         $domPdf = new Dompdf();
         $options = $domPdf->getOptions();
         $options->setDefaultFont('Helvetica');
         $domPdf->setOptions($options);
-        $domPdf->setPaper('A4', 'landscape');
+        $domPdf->setPaper('A4', 'portrait');
     }
 
     public function showPdfFile($html): void
