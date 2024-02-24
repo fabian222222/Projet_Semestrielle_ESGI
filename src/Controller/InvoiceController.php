a99252376f419c538593fdd769b9405d8ba0db47
Sébastien <sebramirez93100@gmail.com>
2024-02-24 19:24:07 +0100

    AE-44 : Création espace comptable - Création requete back

--- src/Controller/InvoiceController.php
+++ src/Controller/InvoiceController.php
@@ -21,7 +21,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Knp\Bundle\SnappyBundle\Snappy\Response\PdfResponse;
 
 
 #[Route('/invoice')]
