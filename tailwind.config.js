ac5ba225ae902fd1d3618cf3b798f1a92e077ac4
Sébastien <sebramirez93100@gmail.com>
2024-03-03 06:19:30 +0100

    AE-96 : Ajoute du dark et variable des couleurs

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,23 @@ module.exports = {
     extend: {
       colors: {
         'red-primary': '#EA3E2B',
-        'blue-primary': '#21053D'
+        'red-secondary' : '#ef4444',
+        'blue-primary': '#21053D',
+        'black': '#000',
+
+        'primary': '#FFFFFF',
+        'secondary': '#F5F7FA',
+        'third': '#cfd1d9',
+        'four': '#E2E2E2',
+
+
+        'dark-mode-primary': '#1D1D1D',
+        'dark-mode-secondary': '#202020',
+        'dark-mode-third': '#292929',
+        'dark-mode-four': '#363434',
+        'dark-mode-five': '#413f3f',
+        'dark-light-purple': '#7058DC',
+        'dark-dark-purple': '#512d8d',
       }
     },
   },
